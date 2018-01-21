45e4a16ad5ec164f0af53fb4ff05140114b1a7a1
Jose Alvarez <jp.alvarezl@gmail.com>
2018-01-21 09:47:10 +0100

    Project created

---
+++ lib/twitter_search.ex
@@ -0,0 +1,18 @@
+defmodule TwitterSearch do
+  @moduledoc """
+  Documentation for TwitterSearch.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TwitterSearch.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/twitter_search_test.exs
@@ -0,0 +1,8 @@
+defmodule TwitterSearchTest do
+  use ExUnit.Case
+  doctest TwitterSearch
+
+  test "greets the world" do
+    assert TwitterSearch.hello() == :world
+  end
+end
