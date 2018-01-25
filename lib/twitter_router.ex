9109068d9cebccf2052a9708f0201f23b5ac6d55
Jose Alvarez <jp.alvarezl@gmail.com>
2018-01-25 12:57:02 +0100

    Set up plug and serving of static html

---
+++ lib/twitter_router.ex
@@ -0,0 +1,23 @@
+defmodule TwitterRouter do
+  use Plug.Router
+
+  plug Plug.Static, at: "/", from: :server
+  plug :match
+  plug :dispatch
+
+  def init(options) do
+    # initialize options
+
+    options
+  end
+
+  get "/" do
+    conn
+    |> put_resp_content_type("text/html")
+    |> send_file(200, "priv/static/index.html")
+  end
+
+  match _ do
+    send_resp(conn, 404, "not found")
+  end
+end
