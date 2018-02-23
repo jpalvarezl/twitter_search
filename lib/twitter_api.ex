f753948f0f31026143e79169d6c95a8d30305818
Jose Alvarez <jp.alvarezl@gmail.com>
2018-02-23 11:33:26 +0100

    Tweet querying to API works

--- lib/twitter_api.ex
+++ lib/twitter_api.ex
@@ -25,12 +25,20 @@ defmodule TwitterApi do
     |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
   end
 
-  # def get_twitts do
-  #   token = "some_token_from_another_request"
-  #   url = "https://api.twitter.com/1.1/search/tweets.json"
-  #   headers = ["Authorization": "Bearer #{token}", "Accept": "Application/json; Charset=utf-8"]
-  #   options = [ssl: [{:versions, [:'tlsv1.2']}], recv_timeout: 500]
-  #   {:ok, response} = HTTPoison.get(url, headers, options)
-  #   IO.inspect response.body
-  # end
+  def get_tweets(search_term) do
+    token_json = post_auth()
+    token = token_json[:access_token]
+    url = "https://api.twitter.com/1.1/search/tweets.json?q="<>search_term
+    headers = ["Authorization": "Bearer #{token}", "Accept": "Application/json; Charset=utf-8"]
+    options = [ssl: [{:versions, [:'tlsv1.2']}], recv_timeout: 500]
+    {:ok, response} = HTTPoison.get(url, headers, options)
+    IO.inspect decode_tweets(response.body)
+  end
+
+  def decode_tweets(body) do
+    body
+    |> Poison.decode!
+    # |> Map.take(["token_type", "access_token"])
+    # |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
+  end
 end
